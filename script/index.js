62f549015905e1ce86308e18bc69f9a8fafad8d6
morad-aiachi-dashfile-at-394109254195 <aiachi3u@etu.univ-lorraine.fr>
2025-07-29 03:21:12 +0200

    Typewritting effect ok

---
+++ script/index.js
@@ -0,0 +1,93 @@
+const citySpan = document.getElementById("writing-slogan");
+// citySpan.textContent = "dedze";
+
+// const cities = [
+//   "Metz",
+//   "Nancy",
+//   "Thionville",
+//   "Épinal",
+//   "Vandœuvre-lès-Nancy",
+//   "Montigny-lès-Metz",
+//   "Sarreguemines",
+//   "Saint-Dié-des-Vosges",
+//   "Forbach",
+//   "Lunéville"
+// ];
+// const cities = [
+//   "Metz",
+//   "Nancy",
+//   "Thionville",
+//   "Épinal",
+//   "Vandœuvre",
+//   "Sarreguemines",
+//   "Saint-Dié-des-Vosges",
+//   "Forbach",
+//   "Lunéville"
+// ];
+// const villesSimplesLorraine = [
+//   "Metz",
+//   "Nancy",
+//   "Thionville",
+//   "Épinal",
+//   "Forbach",
+//   "Longwy",
+//   "Hayange",
+//   "Commercy",
+//   "Bar-le-Duc", 
+//   "Sarrebourg"  
+// ];
+const cities = [
+    "Neuves-Maisons",
+    "Nancy",
+    "Metz",
+    "Thionville",
+    "Épinal",
+    "Forbach",
+    "Longwy",
+    "Hayange",
+    "Commercy",
+    "Sarrebourg",
+    "Neufchâteau"
+];
+
+// const conjonction = [
+//   "Mais",
+//   "Ou",
+//   "Est",
+//   "Donc",
+//   "Or",
+//   "Ni",
+//   "Car"
+// ];
+console.log(cities[0][cities[0].length-1]);
+
+
+function typeWrite(buffer, elements, currentElement, currentLetter, deleting) {
+  var nbElements = elements.length - 1;
+    if(!deleting){
+        if ( currentLetter <= elements[currentElement].length - 1) {
+            buffer.textContent = elements[currentElement].substring(0, currentLetter + 1);
+            setTimeout(typeWrite,150,buffer, elements, currentElement, currentLetter + 1, deleting);
+        }else{
+            deleting=true;
+            currentLetter-=1;
+            setTimeout(typeWrite,300,buffer, elements, currentElement, currentLetter + 1, deleting);
+        }
+    }else{
+        if(currentLetter >=0){
+            buffer.textContent = elements[currentElement].substring(0, currentLetter );
+            currentLetter-=1;
+            setTimeout(typeWrite,100,buffer, elements, currentElement, currentLetter, deleting);
+
+        }else{
+            deleting = false;
+            //On passe au mot suivant : 
+            currentElement = (currentElement + 1) % (elements.length);
+            currentLetter = 0;
+            setTimeout(typeWrite,200,buffer, elements, currentElement, currentLetter, deleting);
+        }
+    }
+
+}
+
+typeWrite(citySpan, cities, 0, 0, false);
